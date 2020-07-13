0dbce5dce77b4cbdaeb99292ca1cd8020086faeb
Abtahee Ali <abtaheeali@gmail.com>
2020-07-13 16:05:23 -0400

    footer queries for min width 1270

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -13,7 +13,7 @@ export function App() {
           <article>
             <a href="#">
               <img
-                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                src="https://i.pcmag.com/imagery/articles/03q56RTgHk4cQvs07pQt4I9-1.fit_scale.size_2698x1517.v1569490700.jpg"
                 width="80rem"
                 height="80rem"
                 alt="GHLogo"
@@ -22,22 +22,22 @@ export function App() {
           </article>
           <article>
             <div className="gamer-card-text">
-              <h3 className="gamer-card-text-first-item">Name</h3>
-              <h3>Gamerscore</h3>
+              <h3 className="gamer-card-text-first-item">Cody Banks</h3>
+              <h3>G 46994</h3>
             </div>
-            <h2>GamerTag</h2>
+            <h2>UNPROFESSI0NAL</h2>
             <div className="gamer-card-text">
               <div className="gamer-card-ff">
                 <h4 className="gamer-card-text-first-item">Friends</h4>
-                <p className="gamer-card-text-first-item">#</p>
+                <h4 className="gamer-card-text-first-item"># 136</h4>
               </div>
               <div className="gamer-card-ff">
                 <h4>Followers</h4>
-                <p>#</p>
+                <h4>#</h4>
               </div>
             </div>
-            <h4>Bio</h4>
-            <h4>Location</h4>
+            <h4>Bio: Yo yo yo</h4>
+            <h4>Location: Florida</h4>
           </article>
         </section>
       </body>
