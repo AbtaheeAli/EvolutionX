1abf05936d4fa48f47e8aa9e564a22a9a6863c0d
Abtahee Ali <abtaheeali@gmail.com>
2020-07-13 10:48:01 -0400

    Removed stock components

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,17 +1,6 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router'
-import { Layout } from './components/Layout'
-import { Home } from './pages/Home'
-import HelloWorld from './pages/_template/HelloWorld'
-import HeyWorld from './pages/_template/HeyWorld'
-import NotFound from './pages/NotFound'
+import React from 'react'
 import './custom.scss'
 
-
-export function  App() {
-  static displayName = App.name
-
-  render() {
-    return <p>App!</p>
-  }
+export function App() {
+  return <p>App!</p>
 }
