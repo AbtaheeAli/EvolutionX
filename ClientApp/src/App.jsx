6b25e84c9eb84c0d29e8e43bd9704414883c3cd0
Abtahee Ali <abtaheeali@gmail.com>
2020-07-14 18:08:16 -0400

    created and refactored about page

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -9,6 +9,7 @@ import { Friends } from './pages/Friends'
 import { RecentAchievements } from './pages/RecentAchievements'
 import { Messages } from './pages/Messages'
 import { XboxOneGames } from './pages/XboxOneGames'
+import { About } from './pages/About'
 
 export function App() {
   return (
@@ -33,27 +34,7 @@ export function App() {
             <XboxOneGames />
           </Route>
           <Route path="/about">
-            <section className="about-box">
-              <article>
-                <a href="#">
-                  <img
-                    className="Game"
-                    src="https://i7.pngguru.com/preview/79/769/243/xbox-360-microsoft-studios-fable-legends-xbox-one-xbox-fitness-avatar-thumbnail.jpg"
-                    width="200rem"
-                    height="200rem"
-                    alt="GHLogo"
-                  />
-                </a>
-                <p>
-                  Evolution X is an application that uses an third-party API,
-                  XAPI, to access the users Xbox Live Account. Once the users
-                  are signed in, they are able to view their gamer card, Xbox
-                  Live friends, messages, recent achievement, and their Xbox One
-                  Games.
-                </p>
-                <small>We are not affiliated with Microsoft or XAPI.</small>
-              </article>
-            </section>
+            <About />
           </Route>
         </Switch>
         <Footer />
---
+++ ClientApp/src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export function About() {
+  return (
+    <section className="about-box">
+      <article>
+        <a href="#">
+          <img
+            className="Game"
+            src="https://i7.pngguru.com/preview/79/769/243/xbox-360-microsoft-studios-fable-legends-xbox-one-xbox-fitness-avatar-thumbnail.jpg"
+            width="200rem"
+            height="200rem"
+            alt="GHLogo"
+          />
+        </a>
+        <p>
+          Evolution X is an application that uses an third-party API, XAPI, to
+          access the users Xbox Live Account. Once the users are signed in, they
+          are able to view their gamer card, Xbox Live friends, messages, recent
+          achievement, and their Xbox One Games.
+        </p>
+        <small>We are not affiliated with Microsoft or XAPI.</small>
+      </article>
+    </section>
+  )
+}
