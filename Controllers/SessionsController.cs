a25c641f0fb5dc446d4388611cc632e353524d25
Abtahee Ali <abtaheeali@gmail.com>
2020-07-18 13:28:33 -0400

    Added sessions controller.cs

---
+++ Controllers/SessionsController.cs
@@ -0,0 +1,21 @@
+using EvolutionX.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace EvolutionX.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionsController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        readonly protected string JWT_KEY;
+
+        public SessionsController(DatabaseContext context, IConfiguration config)
+        {
+            _context = context;
+            JWT_KEY = config["JWT_KEY"];
+        }
+    }
+}
