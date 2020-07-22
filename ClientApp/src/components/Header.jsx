0f3e0a52ff580f1e88b6ef1c324fb05bb0105f26
Cody Banks <codydouglasbanks@gmail.com>
2020-07-22 10:09:26 -0400

    added data for user profile to page

--- ClientApp/src/components/Header.jsx
+++ ClientApp/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import classnames from 'classnames'
 
 export function Header() {
   return (
--- ClientApp/src/pages/UserSettings.jsx
+++ ClientApp/src/pages/UserSettings.jsx
@@ -2,5 +2,16 @@ import React, { useState, useEffect } from 'react'
 import { getUser } from '../auth'
 
 export function UserSettings() {
-  return <div></div>
+  const user = getUser()
+
+  return (
+    <section>
+      <h3>{user.userName}</h3>
+      <ul>
+        <li>Email: {user.email}</li>
+        <li>X API Key: {user.apiKey}</li>
+        <li>Xbox Profile User ID: {user.xboxProfileUserId}</li>
+      </ul>
+    </section>
+  )
 }
