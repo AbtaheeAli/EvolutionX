b5a32051fa78e2bfda6fe09f475f8440d7feb3e9
Cody Banks <codydouglasbanks@gmail.com>
2020-07-24 16:52:20 -0400

    fixed loading animation

--- ClientApp/src/pages/Friends.jsx
+++ ClientApp/src/pages/Friends.jsx
@@ -57,31 +57,35 @@ export function Friends(props) {
 
   return (
     <section className="friend-page">
-      <div className="search-bar">
-        {loading === false && (
-          <div className="spinner">
-            <div className="spinner-border text-danger" role="status">
-              <span class="sr-only">Loading...</span>
+      {loading === true && (
+        <div className="search-bar">
+          <form className=" form-inline my-2 my-lg-0">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">
+                  &#x1F50D;
+                </span>
+              </div>
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search For Friend"
+                aria-label="Search"
+                onChange={event => setFilterText(event.target.value)}
+              />
             </div>
+          </form>
+        </div>
+      )}
+
+      {loading === false && (
+        <div className="spinner mt-5 pt-5 d-flex justify-content-center align-items-center">
+          <div className="spinner-border text-danger" role="status">
+            <span class="sr-only">Loading...</span>
           </div>
-        )}
-        <form className=" form-inline my-2 my-lg-0">
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text" id="basic-addon1">
-                &#x1F50D;
-              </span>
-            </div>
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search For Friend"
-              aria-label="Search"
-              onChange={event => setFilterText(event.target.value)}
-            />
-          </div>
-        </form>
-      </div>
+        </div>
+      )}
+
       <div className="friend-cards">
         {friends
           .filter(friend => friend.Gamertag.includes(filterText))
