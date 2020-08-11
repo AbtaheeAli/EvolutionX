8e150adcbbd1789f50e817b5f66587ad58ed7db9
Cody Banks <codydouglasbanks@gmail.com>
2020-08-11 10:10:34 -0400

    added demo data for friends

--- ClientApp/src/pages/Demo/DemoFriends.jsx
+++ ClientApp/src/pages/Demo/DemoFriends.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react'
+import FriendsData from '../../Data/Friends.json'
 
 function SingleFriend(props) {
   return (
     <section className="friend-card">
-      <h4 className="friend-gt">{/* <strong>{props.Gamertag}</strong> */}</h4>
+      <h4 className="friend-gt">
+        <strong>{props.Gamertag}</strong>
+      </h4>
       <article className="friend-card-img">
         <img
           className="friendAvatar"
-          // src={props.GameDisplayPicRaw}
+          src={props.GameDisplayPicRaw}
           width="120rem"
           height="120rem"
           alt="friend"
@@ -19,7 +22,7 @@ function SingleFriend(props) {
             alt="Gamerscore"
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAABmJLR0QA/wD/AP+gvaeTAAAB40lEQVRIib3WPY+MURTA8Z+XFRt2JEswg0SlVElEFDohGoWXD6CTTfZLEKKRqHaMl1az1ZJYCpXKB1Agm0VW0DBjZEkYxX2e7LXmmXtnTfYkp3jOc+7533vuOfde1lk2ZPrVcBoncQCNwr6Ed5jHY7T/d0L7cAc/0EvoMm5HkxlaptHNAK3WLqaGAW0pVjUsaLW2MJYDbI4AVur9FGx6hLBSK9PbsLY9S+l37C8hmyLgTRxNpQAdPMcbYY92JPzHsB0PY2NNuvR/4yrGsVPIyEacxefE2GVMxMCLiQE93MA2zBbwHl7hCI5Htio9HwPvJpzbxQyv9/m3gF14kYjRioFPEs5zhd+XjExU6TxsLgLVDZYF7PF3gXTxM/oex9YBMRqETSf/EI/lEiYjvZXw78XApYTzQXzC18j2wEq6DgkrHCQfYuDbhPMJoUKbff4t4jUOJ2Isxh8XpDf9mtDAs/hV2F4KbXFMui3OxcCa0Jypxr8ipG4Su4uxZ/AxMfafxidcnjnl3cYzPBKqN2fMTL8c1/EtM8Aw2sHefkDCVTJq4OUqWCmjvIDvpWCE66Q1AlhT5hOjlClr29OOjDRWSb2YaaplytKfMaBAyD9DJ3DKykO4fDK8F06pp8JDuJO9lPWSP/40hhcZSgYiAAAAAElFTkSuQmCC"
           />{' '}
-          {/* {props.Gamerscore} */}
+          {props.Gamerscore}
         </h4>
       </article>
     </section>
@@ -56,9 +59,8 @@ export function DemoFriends() {
       </div>
 
       <div className="friend-cards">
-        {/* {friends
-          .filter(friend => friend.Gamertag.includes(filterText))
-          .map(friend => (
+        {FriendsData.filter(friend => friend.Gamertag.includes(filterText)).map(
+          friend => (
             <SingleFriend
               key={friend.id}
               Gamertag={friend.Gamertag}
@@ -67,7 +69,8 @@ export function DemoFriends() {
               TenureLevel={friend.TenureLevel}
               GameDisplayPicRaw={friend.GameDisplayPicRaw}
             />
-          ))} */}
+          )
+        )}
       </div>
     </section>
   )
