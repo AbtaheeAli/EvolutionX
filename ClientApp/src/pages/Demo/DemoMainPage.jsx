4c3252dbd8a132ac7a49568de6879e4ae0058098
Cody Banks <codydouglasbanks@gmail.com>
2020-08-11 11:35:31 -0400

    small fix

--- ClientApp/src/pages/Demo/DemoMainPage.jsx
+++ ClientApp/src/pages/Demo/DemoMainPage.jsx
@@ -3,9 +3,7 @@ import { SideNav } from '../../components/SideNav'
 import { Header } from '../../components/Header'
 import { Route, Switch } from 'react-router-dom'
 import { Footer } from '../../components/Footer'
-
 import { About } from '../../pages/About'
-
 import { DemoGamercard } from './DemoGamerCard'
 import { DemoFriends } from './DemoFriends'
 import { DemoMessages } from './DemoMessages'
