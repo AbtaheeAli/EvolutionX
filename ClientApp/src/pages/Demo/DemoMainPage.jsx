1202895c3fc2848191827ee3864265d7dcca38fa
Abtahee Ali <abtaheeali@gmail.com>
2020-08-12 11:51:10 -0400

    Added demo header to demo page

--- ClientApp/src/pages/Demo/DemoMainPage.jsx
+++ ClientApp/src/pages/Demo/DemoMainPage.jsx
@@ -9,12 +9,13 @@ import { DemoFriends } from './DemoFriends'
 import { DemoMessages } from './DemoMessages'
 import { DemoRecentAchievements } from './DemoRecentAchievements'
 import { DemoXboxOneGames } from './DemoXboxOneGames'
+import { DemoHeader } from './DemoHeader'
 
 export default function DemoMainPage() {
   return (
     <div className="main">
       <DemoSideNav />
-      <Header />
+      <DemoHeader />
       <body>
         <Switch>
           <Route exact path="/demo-gamercard">
