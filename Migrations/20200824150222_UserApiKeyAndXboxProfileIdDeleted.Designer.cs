f8f663627f0a98f6637155fb83ecac1f4bd335e3
Cody Banks <codydouglasbanks@gmail.com>
2020-08-24 11:03:59 -0400

    added migration to user

--- Migrations/20200824150222_UserApiKeyAndXboxProfileIdDeleted.Designer.cs
+++ Migrations/20200824150222_UserApiKeyAndXboxProfileIdDeleted.Designer.cs
@@ -2,15 +2,17 @@
 using EvolutionX.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace EvolutionX.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200824150222_UserApiKeyAndXboxProfileIdDeleted")]
+    partial class UserApiKeyAndXboxProfileIdDeleted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200824150222_UserApiKeyAndXboxProfileIdDeleted.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EvolutionX.Migrations
+{
+    public partial class UserApiKeyAndXboxProfileIdDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiKey",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "XboxProfileUserId",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApiKey",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "XboxProfileUserId",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
