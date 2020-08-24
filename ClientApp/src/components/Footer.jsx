6c4d31fda565d9db6dd8b64bfdf84ad81fdf036b
Abtahee Ali <abtaheeali@gmail.com>
2020-08-24 10:27:08 -0400

    Fixed grammar in footer

--- ClientApp/src/components/Footer.jsx
+++ ClientApp/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer>
       <p>© 2020 Evolution X - Created and owned by Cody Banks & Abtahee Ali</p>
       <p>
-        xapi.us is an unofficial API & Evolution X it is in no way endorsed or
+        xapi.us is an unofficial API & Evolution X is in no way endorsed or
         affiliated to the Microsoft Corporation, Xbox, Xbox LIVE and any Xbox
         images are registered trademarks of their respected owners.
       </p>
