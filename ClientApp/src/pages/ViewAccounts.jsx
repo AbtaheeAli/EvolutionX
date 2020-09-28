9102df84783877dc4272f811be281711734f1f9d
Cody Banks <codydouglasbanks@gmail.com>
2020-09-28 14:58:05 -0400

    useeffect for each individual account

--- ClientApp/src/pages/ViewAccounts.jsx
+++ ClientApp/src/pages/ViewAccounts.jsx
@@ -11,15 +11,47 @@ export function UserAccount(props) {
 
   const [showEmail, setShowEmail] = useState(false)
 
+  const [xboxAccount, setXboxAccount] = useState()
+  console.log(xboxAccount)
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    function loadAccountInfo() {
+      const url = `https://xapi.us/v2/${props.xboxProfileUserId}/new-profile`
+
+      fetch(url, {
+        method: 'GET',
+        headers: {
+          'X-AUTH': props.apiKey,
+        },
+      })
+        .then(response => response.json())
+        .then(account => {
+          setXboxAccount(account)
+          setLoading(true)
+        })
+    }
+
+    loadAccountInfo()
+  }, [props.apiKey, props.xboxProfileUserId])
+
   return (
     <div className="account-settings-card viewAccount">
+      {loading === false && (
+        <div className="spinner mt-5 pt-5 d-flex justify-content-center align-items-center">
+          <div className="spinner-border text-danger" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      )}
+
       <ul className="settings-list">
         <li>
           <h3 className="user-settings-username accountUsername">
             {props.accountName}
           </h3>
         </li>
-
         <li>
           <button
             className="btn settings-button"
@@ -48,6 +80,32 @@ export function UserAccount(props) {
           {showXboxId && <div>{props.xboxProfileUserId}</div>}
         </li>
       </ul>
+
+      {loading === true && (
+        <div>
+          <div className="gamer-card-img-container">
+            <img
+              className="gamer-card-img"
+              src={xboxAccount.displayPicRaw}
+              width="280rem"
+              height="280rem"
+              alt="profile"
+            />
+          </div>
+          <h2 className="gamercard-gamertag">{xboxAccount.gamertag}</h2>
+          <h3 className="user-settings-username accountUsername">
+            {xboxAccount.realName}
+          </h3>
+          <h3>
+            {' '}
+            <img
+              alt="gamerscore"
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAACF0lEQVRIic3Xu2sUURTH8U9MYjaIEDWYh88YxSKCiCBYWfgofIB/gHZiYeEfoYX4CEGEdCKipai9KBGMiNhIRBsfINF0KYxLEkFjcWeWybrZuRN30R+cYu7Mud87Z8459w7/SC0Fnu3EIRxAHzYm45P4iqd4jNlGLW4Qd1DGQo59x20M/A2whBHMRwCrbR7D6CgK7RZCVxRYbc/RGwvtxacGQFP7iJ48aClZZaOgqY3LCftIE6CpXVsKOmB5iVQk4QZTWGsGfAN76oUjURk3k+fvYwKbsSbHrxVdeJgd7BRqMG/V77AF63AaZ7Eb7RiN8J8R8qiiExFOc9iJfZiuuncpeaOXEfMcy4KvRjjcFVrsqxr3fuGkkEB581yBtgS8Qb4eYRf21rjXggcRc1RYKbgvwuEztleNlfEjc92G1Tnz9MOKCGA9ncPajB2J8FnIgqciHDYJ7S+rUSHRpnFK3KYwlQV/iXA4LNTs+8zYKqF+u/BayPo8TWYvjosrpx3Yb3E5/cTF5CVeRMxzNAuObSBvhJCnDeQMhoQavh7hX2kgaahnhfaXpyG8xYXk+ptQvxM4H+F/T4jcIm1NBpu5SWxbakXDTQRfrheKdow1AfpMRKn1CPXaKOgHrM+Dpupu0JuPizhvVatD2G2Wk3BzwjddWRSa1YBwSI+p8xncEg4LdVXkF6aEg8IvTL8/f2HG8ESNOv2v9BvNTaDnqkETWAAAAABJRU5ErkJggg=="
+            />{' '}
+            {xboxAccount.gamerScore}
+          </h3>
+        </div>
+      )}
     </div>
   )
 }
@@ -58,27 +116,10 @@ export function ViewAccounts() {
   const [errorMessage, setErrorMessage] = useState()
 
   const [accounts, setAccounts] = useState()
-  console.log(accounts)
 
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    // function loadAccountInfo() {
-    //   const url = `https://xapi.us/v2/${user.xboxProfileUserId}/new-profile`
-
-    //   fetch(url, {
-    //     method: 'GET',
-    //     headers: {
-    //       'X-AUTH': user.apiKey,
-    //     },
-    //   })
-    //     .then(response => response.json())
-    //     .then(account => {
-    //       setXboxAccount(account)
-    //       setLoading(true)
-    //     })
-    // }
-
     const fetchUser = async () => {
       const response = await fetch(`/api/Accounts/UserId/${user.id}`)
       const apiData = await response.json()
@@ -86,7 +127,6 @@ export function ViewAccounts() {
       setLoading(true)
     }
 
-    // loadAccountInfo()
     fetchUser()
   }, [user.id])
 
